fe550604037b33d0bc7b444fabf90255bb9c06a4
eunyoung kim <rola70@nate.com>
2018-12-02 15:34:50 +0900

    type4

--- HT502-imageSlider_template/js/type2.js
+++ HT502-imageSlider_template/js/type2.js
@@ -1,7 +1,9 @@
-$(function(){
-    $("#slider ul li.item:gt(0)").hide();
+$(document).ready(function () {
+    // 모든 슬라이드 숨김
+    $('#slider ul li:gt(0)').hide();
 
-    setInterval(function(){
-        $("#slider ul li.item:first").fadeOut(1000).next().fadeIn(1000).end().appendTo( $("#slider ul"));        
-    },2000);
+    // 첫번째 슬라이드 아이템에 대해 반복을 실행
+    setInterval(() => {
+        $('#slider ul .item:first').fadeOut(1000).next().fadeIn(1000).end().appendTo('#slider ul');
+    }, 3000);
 });
