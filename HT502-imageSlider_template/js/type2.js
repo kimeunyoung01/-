7139487c93c5fdac95483b2156397059982bb019
eunyoung kim <rola70@nate.com>
2018-12-02 14:21:54 +0900

    두번째 예제

---
+++ HT502-imageSlider_template/js/type2.js
@@ -0,0 +1,7 @@
+$(function(){
+    $("#slider ul li.item:gt(0)").hide();
+
+    setInterval(function(){
+        $("#slider ul li.item:first").fadeOut(1000).next().fadeIn(1000).end().appendTo( $("#slider ul"));        
+    },2000);
+});
